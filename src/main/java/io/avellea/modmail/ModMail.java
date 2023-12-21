7148a03a4ccaeec93d55e0a7e4b3d1dd6e73061a
Avellea <dynamicdonkey0@gmail.com>
2023-12-21 15:37:35 -0600

    Removed bungee dependency, moved to bukkit.

--- src/main/java/io/avellea/modmail/ModMail.java
+++ src/main/java/io/avellea/modmail/ModMail.java
@@ -13,7 +13,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import net.md_5.bungee.api.ChatColor;
+import org.bukkit.ChatColor;
 
 public class ModMail implements CommandExecutor {
 
